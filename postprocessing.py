2816a8d45db6e90782c5e6da43e3e474641a8220
LonelyQuantum <pengshenglin2018@gmail.com>
2020-05-05 21:30:13 -0500

    Selector fix and date extraction
    1. Fix the selector for Shenzhen
    2. Use regex to match date
    3. Normal testing

--- postprocessing.py
+++ postprocessing.py
@@ -5,9 +5,12 @@ Usage: from postprocess import *
 import pandas as pd
 import json
 import dateparser
+import re
 
 json_path = 'results/json/'
 csv_path = 'results/csv/'
+
+date_pattern = '([0-9]+[-年月日/0-9\.]*[0-9]+[日]?)'
 
 def load_json(path):
     with open(path) as file:
@@ -19,8 +22,15 @@ def result_format(city):
     cols = res_json[0].keys()
     res_csv = pd.DataFrame(columns=cols)
     for item in res_json:
-        item['date'] = dateparser.parse(item['date'].strip('(),.\n\ '))
+        date_cur = re.findall(date_pattern,item['date'])
+        item['date'] = dateparser.parse(date_pattern)
         res_csv = res_csv.append(item, ignore_index=True)
     res_csv['date'] = pd.to_datetime(res_csv['date'])
     res_csv = res_csv.sort_values(by='date')
     res_csv.to_csv(csv_path + city + '.csv',encoding='utf_8_sig')
+
+    
+def change_selector(city,columns,values):
+    selector_settings = pd.read_csv('settings/selector_settings.csv',index_col='city')
+    selector_settings.loc[city][columns] = values
+    selector_settings.to_csv('settings/selector_settings.csv',encoding='utf_8_sig')
