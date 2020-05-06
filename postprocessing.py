e9a702bda4c14f740139ef94b21d9c7f01a145f2
LonelyQuantum <pengshenglin2018@gmail.com>
2020-05-05 22:17:30 -0500

    Encoding CSV and Update Changchun Selector
    1. Fix the bug in date_extraction regarding null values.
    2. Add a function to re-encode all csv files into 'utf_8_sig'.
    3. Update the news_selector for Changchun
    4. Fix the bug in JSON to CSV conversion where the index column is saved.
    5. Move the previous pre_post_processing.ipynb to another archive directory that we do not usually use.

--- postprocessing.py
+++ postprocessing.py
@@ -6,6 +6,7 @@ import pandas as pd
 import json
 import dateparser
 import re
+import os
 
 json_path = 'results/json/'
 csv_path = 'results/csv/'
@@ -22,12 +23,25 @@ def result_format(city):
     cols = res_json[0].keys()
     res_csv = pd.DataFrame(columns=cols)
     for item in res_json:
-        date_cur = re.findall(date_pattern,item['date'])
-        item['date'] = dateparser.parse(date_cur[0])
+        if type(item['date']) is str:
+            date_cur = re.findall(date_pattern,item['date'])
+            if len(date_cur) > 0:
+                item['date'] = dateparser.parse(date_cur[0])
         res_csv = res_csv.append(item, ignore_index=True)
     res_csv['date'] = pd.to_datetime(res_csv['date'])
     res_csv = res_csv.sort_values(by='date')
-    res_csv.to_csv(csv_path + city + '.csv',encoding='utf_8_sig')
+    res_csv.to_csv(csv_path + city + '.csv',encoding='utf_8_sig',index=False)
+
+def encodeCSV():
+    csvWithIndex = ['settings/selector_settings.csv','settings/city_govs.csv']
+    for csvFile in csvWithIndex:
+        df = pd.read_csv(csvFile,index_col='city')
+        df.to_csv(csvFile,encoding='utf_8_sig')
+    csvWithoutIndex = os.listdir(csv_path)
+    for csvFile in csvWithoutIndex:
+        if csvFile.endswith('.csv'):
+            df = pd.read_csv(csv_path + csvFile)
+            df.to_csv(csv_path + csvFile,index=False,encoding='utf_8_sig')
 
     
 def change_selector(city,columns,values):
